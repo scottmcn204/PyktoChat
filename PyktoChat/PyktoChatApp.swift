726825494c4914f183031cfd3487cf5785a9c1e8
scottmcn204 <mcnallsc@tcd.ie>
2023-01-13 13:29:08 +0000

    Initial Commit

---
+++ PyktoChat/PyktoChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  PyktoChatApp.swift
+//  PyktoChat
+//
+//  Created by Scott McNally on 13/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct PyktoChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
